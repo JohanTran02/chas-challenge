e8e4504dad9ffd15833c952f9bd26d5110f20b11
Isak Tilahun <you@example.com>
2024-05-01 12:07:55 +0200

    Redesigned the dashboard to match the Figma wireframe version.

--- app/ui/dashboard/SideNav.tsx
+++ app/ui/dashboard/SideNav.tsx
@@ -1,10 +1,10 @@
 'use client'; 
 
 // Icons
-import HomeIcon from '@/public/home.svg'; 
-import MapIcon from '@/public/map-pin-alt.svg'; 
-import StampsIcon from '@/public/painting-landscape-framed.svg'; 
-import ProfileIcon from '@/public/profile-1336.svg'; 
+import HomeIcon from '@/public/home-filled.svg'; 
+import MapIcon from '@/public/Map Marker.svg'; 
+import StampsIcon from '@/public/Trophy.svg'; 
+import ProfileIcon from '@/public/Customer.svg'; 
 
 // Components
 import Link from 'next/link'
@@ -15,33 +15,35 @@ export default function SideNav(){
   const path = usePathname();
   const links = [
     {name: 'Home', href: '/dashboard', src: HomeIcon, alt: 'Home icon'},
-    {name: 'Map', href: '/dashboard/map', src: MapIcon, alt: 'Map icon'}, 
     {name: 'Achievements', href: '/dashboard/achievements', src: StampsIcon, alt: 'Stamps Icon'},
+    {name: 'Map', href: '/dashboard/map', src: MapIcon, alt: 'Map icon'}, 
     {name: 'Profile', href: '/dashboard/profile', src: ProfileIcon, alt: 'Profile icon'}
   ]
 
   return (
-    <ul className='h-24 w-full bg-pink-500 flex items-center gap-2 px-4'>    
-      {links.map((link) => {
-        return (
-          <Link 
-            key={link.name}
-            href={link.href}
-            className={`bg-white flex-grow h-10 rounded-md grid place-items-center ${path === link.href && 'bg-blue-200'}`}
-            >
-            <li>
-              <Image 
-                rel='icon' 
-                height={24}
-                width={24}
-                src={link.src} alt={link.alt} 
-                className="object-cover object-center" 
-              />
-            </li>
-          </Link>
-        )
-      })}
-    </ul>
+    <div className='h-24 w-full flex items-start justify-center'>
+      <ul className='h-max w-5/6 border-2 border-black rounded-3xl flex items-center gap-2 px-4'>
+        {links.map((link) => {
+          return (
+            <Link
+              key={link.name}
+              href={link.href}
+              className={` flex-grow h-10 rounded-md grid place-items-center ${/* path === link.href && 'bg-blue-200' */''}`}
+              >
+              <li>
+                <Image
+                  rel='icon'
+                  height={32}
+                  width={32}
+                  src={link.src} alt={link.alt}
+                  className="object-cover object-center"
+                />
+              </li>
+            </Link>
+          )
+        })}
+      </ul>
+    </div>
   )
 }
 
