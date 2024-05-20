aecb2f696af8edfcf8f39de5516a01fbcc935b3a
JohanTran <johan.tran0210@gmail.com>
2024-05-20 22:02:26 +0200

    changed settings

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,12 @@
 
 const nextConfig = {
     reactStrictMode: true,
-    // basePath: process.env.NODE_ENV === 'production' ? '/chas-challenge' : '',
+    basePath: process.env.NODE_ENV === 'production' ? '/chas-challenge' : '',
     trailingSlash: true,
     output: "export",
+    images: {
+        unoptimized: true,
+    },
 };
 
 export default nextConfig;
