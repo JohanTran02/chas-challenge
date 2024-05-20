127605ec1536f9300f309cc77e50ef1117d6e49f
JohanTran <johan.tran0210@gmail.com>
2024-05-20 22:35:58 +0200

    test

--- app/page.tsx
+++ app/page.tsx
@@ -22,6 +22,7 @@ export default function Home() {
       <h1 className={`text-4xl font-bold text-darkGreen text-center ${poppins.className}`}>
         <span className='block pb-4'>OUTDOOR</span>
         <span className='block'>EXPLORER</span>
+        <span className='block'>EXPLORER</span>
       </h1>
     </div>
   );
