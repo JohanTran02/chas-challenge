d46b88f104c91b6d4280806050354198754b7ec1
JohanTran <johan.tran0210@gmail.com>
2024-05-22 14:31:42 +0200

    token in header

--- cookies.ts
+++ cookies.ts
@@ -1,13 +1,25 @@
-'use server'; 
+'use server';
 
 import { cookies } from 'next/headers'
 import { cookiesForUser } from './app/lib/definitions';
 
-export const createCookie = async ({user}: {user: cookiesForUser}) => { 
+export const createCookie = async ({ user }: { user: cookiesForUser }) => {
   cookies().set({
     name: 'Session',
     value: JSON.stringify(user),
     httpOnly: true,
     path: '/',
   })
+}
+
+export const getCookie = async (name: "Session") => {
+  const cookie = cookies().get(name);
+
+  if (cookie) {
+    const value = cookie.value;
+
+    return JSON.parse(value) as cookiesForUser;
+  }
+
+  return;
 }
