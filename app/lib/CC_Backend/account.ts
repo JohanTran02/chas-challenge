4d3905ef78bd7e1dc3a26cb2290b56395d092170
JohanTran <johan.tran0210@gmail.com>
2024-05-23 17:00:57 +0200

    commented futher code for debugging

--- app/lib/CC_Backend/account.ts
+++ app/lib/CC_Backend/account.ts
@@ -1,35 +1,35 @@
-import { AccountEndpoint, UserValues } from "../definitions";
+// import { AccountEndpoint, UserValues } from "../definitions";
 
-export const account = async(endpoint: AccountEndpoint, userInfo: UserValues) => {
-  let json; 
-  let code; 
+// export const account = async(endpoint: AccountEndpoint, userInfo: UserValues) => {
+//   let json; 
+//   let code; 
   
-  try {
-    const response = await fetch('https://natureai.azurewebsites.net/' + endpoint, {
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      method: "POST",
-      body: JSON.stringify(userInfo)
-    });
+//   try {
+//     const response = await fetch('https://natureai.azurewebsites.net/' + endpoint, {
+//       headers: {
+//         'Content-Type': 'application/json'
+//       },
+//       method: "POST",
+//       body: JSON.stringify(userInfo)
+//     });
 
-    if(response.status === 200){
-      json = await response.json();
-      code = response.status; 
-      console.log('Status code is 200 and the fetching proccess has been successfully completed!', json)
-    }
+//     if(response.status === 200){
+//       json = await response.json();
+//       code = response.status; 
+//       console.log('Status code is 200 and the fetching proccess has been successfully completed!', json)
+//     }
     
-    if(!response.ok){
-      json = await response.json();
-      const errorMessage: {code: string; description: string} = await json[0];
+//     if(!response.ok){
+//       json = await response.json();
+//       const errorMessage: {code: string; description: string} = await json[0];
 
-      console.log(response.status, response.statusText, '- json response:', json) // on error
-      alert(errorMessage.description);
-    }
+//       console.log(response.status, response.statusText, '- json response:', json) // on error
+//       alert(errorMessage.description);
+//     }
 
-  } catch (error) {
-    console.error('Something went wrong fetching data:', error); 
-  }
+//   } catch (error) {
+//     console.error('Something went wrong fetching data:', error); 
+//   }
   
-  return {code, json};
-}
+//   return {code, json};
+// }
--- app/lib/CC_Backend/camera.ts
+++ app/lib/CC_Backend/camera.ts
@@ -1,44 +1,44 @@
-import { getCookie } from "@/cookies";
-import { CameraEndpoint } from "../definitions";
+// import { getCookie } from "@/cookies";
+// import { CameraEndpoint } from "../definitions";
 
-export const camera = async (endpoint: CameraEndpoint, base64: string) => {
-    let json;
-    let code;
+// export const camera = async (endpoint: CameraEndpoint, base64: string) => {
+//     let json;
+//     let code;
 
-    const session = await getCookie("Session");
-    let token;
+//     const session = await getCookie("Session");
+//     let token;
 
-    if (session) {
-        token = session.accessToken;
-    }
+//     if (session) {
+//         token = session.accessToken;
+//     }
 
-    try {
-        const response = await fetch('https://natureai.azurewebsites.net/' + endpoint, {
-            headers: {
-                'Content-Type': 'application/json',
-                Authorization: `Bearer ${token}`
-            },
-            method: "POST",
-            body: JSON.stringify({ prompt: "tree", picture: base64 })
-        });
+//     try {
+//         const response = await fetch('https://natureai.azurewebsites.net/' + endpoint, {
+//             headers: {
+//                 'Content-Type': 'application/json',
+//                 Authorization: `Bearer ${token}`
+//             },
+//             method: "POST",
+//             body: JSON.stringify({ prompt: "tree", picture: base64 })
+//         });
 
-        if (response.status === 200) {
-            json = await response.json();
-            code = response.status;
-            console.log('Status code is 200 and the fetching proccess has been successfully completed!', json)
-        }
+//         if (response.status === 200) {
+//             json = await response.json();
+//             code = response.status;
+//             console.log('Status code is 200 and the fetching proccess has been successfully completed!', json)
+//         }
 
-        if (!response.ok) {
-            json = await response.json();
-            const errorMessage: { code: string; description: string } = await json[0];
+//         if (!response.ok) {
+//             json = await response.json();
+//             const errorMessage: { code: string; description: string } = await json[0];
 
-            console.log(response.status, response.statusText, '- json response:', json) // on error
-            alert(errorMessage.description);
-        }
+//             console.log(response.status, response.statusText, '- json response:', json) // on error
+//             alert(errorMessage.description);
+//         }
 
-    } catch (error) {
-        console.error('Something went wrong fetching data:', error);
-    }
+//     } catch (error) {
+//         console.error('Something went wrong fetching data:', error);
+//     }
 
-    return { code, json };
-}
+//     return { code, json };
+// }
