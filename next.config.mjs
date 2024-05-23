963c5b33f17d8a06a21fa86a2c8eea998ea575b5
JohanTran <johan.tran0210@gmail.com>
2024-05-23 16:38:59 +0200

    change configs

--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,12 @@
 const nextConfig = {
     reactStrictMode: true,
     basePath: process.env.NODE_ENV === 'production' ? '/chas-challenge' : '',
-    trailingSlash: true,
     // output: "export",
     images: {
         unoptimized: true,
+    },
+    env: {
+        NEXT_PUBLIC_MAPBOX_TOKEN: process.env.NEXT_PUBLIC_MAPBOX_TOKEN
     },
     optimizeFonts: false
 };
