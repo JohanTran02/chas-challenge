bbaf1e543fa73ad0f6af56d8672994931258d408
JohanTran <johan.tran0210@gmail.com>
2024-05-23 17:55:46 +0200

    commented middleware and cookies

--- cookies.ts
+++ cookies.ts
@@ -1,37 +1,37 @@
-'use server';
+// 'use server';
 
-import { cookies } from 'next/headers'
-import { cookiesForUser } from './app/lib/definitions';
-// import { RequestCookie } from 'next/dist/compiled/@edge-runtime/cookies';
+// import { cookies } from 'next/headers'
+// import { cookiesForUser } from './app/lib/definitions';
+// // import { RequestCookie } from 'next/dist/compiled/@edge-runtime/cookies';
 
-export const createCookie = async ({ user }: { user: cookiesForUser }) => {
-    cookies().set({
-        name: 'Session',
-        value: JSON.stringify(user),
-        httpOnly: true,
-        path: '/chas-challenge',
-    })
-}
+// export const createCookie = async ({ user }: { user: cookiesForUser }) => {
+//     cookies().set({
+//         name: 'Session',
+//         value: JSON.stringify(user),
+//         httpOnly: true,
+//         path: '/chas-challenge',
+//     })
+// }
 
-export const getCookie = (name: string) => {
-    const cookie = cookies().get(name);
+// export const getCookie = (name: string) => {
+//     const cookie = cookies().get(name);
 
-    if (cookie) {
-        const value = cookie.value;
-        console.log(JSON.parse(value))
-        return JSON.parse(value);
-    }
+//     if (cookie) {
+//         const value = cookie.value;
+//         console.log(JSON.parse(value))
+//         return JSON.parse(value);
+//     }
 
-    return;
-}
+//     return;
+// }
 
-export const getCookieToken = (name: 'Session') => {
-    const cookie = cookies().get(name);
+// export const getCookieToken = (name: 'Session') => {
+//     const cookie = cookies().get(name);
 
-    if (cookie) {
-        const value = cookie.value;
-        return value;
-    }
+//     if (cookie) {
+//         const value = cookie.value;
+//         return value;
+//     }
 
-    return // undefined
-}
+//     return // undefined
+// }
