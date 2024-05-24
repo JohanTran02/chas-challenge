776c63fbf90e4fd5045b751fa99f2359834307c7
JohanTran <johan.tran0210@gmail.com>
2024-05-24 12:01:45 +0200

    changed components to server side

--- app/ui/Components/signin/SignIn.tsx
+++ app/ui/Components/signin/SignIn.tsx
@@ -16,7 +16,6 @@ import GetStarted from "./GetStarted";
 import { AppDispatch, RootState } from "@/app/lib/redux/store";
 import { useDispatch, useSelector } from "react-redux";
 import { account } from "@/app/lib/CC_Backend/account";
-// import { createCookie } from "@/cookies";
 
 
 export default function SignIn() {
