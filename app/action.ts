eae29309404c8e77f6639b21ea00d2bbd91390a7
JohanTran <johan.tran0210@gmail.com>
2024-05-26 01:36:30 +0200

    removed export

--- app/action.ts
+++ app/action.ts
@@ -1,16 +1,16 @@
-"use server"
+// "use server"
 
-import { cookies } from "next/headers";
+// import { cookies } from "next/headers";
 
-export const createCookie = async function (): Promise<void> {
-    cookies().set({
-        name: 'Session',
-        value: "test",
-        httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        // domain: "https://johantran02.github.io/chas-challenge",
-        maxAge: 60 * 60 * 24 * 365 * 1000,
-        sameSite: "none",
-        path: '/',
-    })
-}
+// export const createCookie = async function (): Promise<void> {
+//     cookies().set({
+//         name: 'Session',
+//         value: "test",
+//         httpOnly: true,
+//         secure: process.env.NODE_ENV === "production",
+//         // domain: "https://johantran02.github.io/chas-challenge",
+//         maxAge: 60 * 60 * 24 * 365 * 1000,
+//         sameSite: "none",
+//         path: '/',
+//     })
+// }
