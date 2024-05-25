6b1691928df872a39fd2f77a3cc04d139227e3d6
JohanTran <johan.tran0210@gmail.com>
2024-05-26 00:39:14 +0200

    set cookie in page

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,9 +1,13 @@
 import SignIn from "@/app/ui/Components/signin/SignIn";
 import { cookies } from "next/headers";
 
-export default function Page() {
-    const cookieStore = cookies();
-    cookieStore.set("test", "test");
+export default async function Page() {
+    const setCookie = async () => {
+        const cookieStore = cookies();
+        cookieStore.set("test", "test");
+    }
+
+    await setCookie();
     return (
         <>
             <SignIn />
