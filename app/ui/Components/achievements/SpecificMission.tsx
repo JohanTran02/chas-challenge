49ac38948e9a7ac85494e71031c289d11ea613cc
JohanTran <johan.tran0210@gmail.com>
2024-06-03 19:57:00 +0200

    conditional rendering for completed stamps

--- app/ui/Components/achievements/SpecificMission.tsx
+++ app/ui/Components/achievements/SpecificMission.tsx
@@ -6,23 +6,23 @@ import StampStats from './StampStats';
 import ImageHandler from '../../ImageHandler';
 import { useEffect, useState } from 'react';
 
-type Prop = { prop: Stampinfo; completedStamps?: string | undefined }
+type Prop = { stamp: Stampinfo; completedStamps?: string | undefined }
 
-const SpecificMission = ({ prop, completedStamps }: Prop) => {
+const SpecificMission = ({ stamp, completedStamps }: Prop) => {
   const [modal, setModal] = useState<Boolean>(false);
 
   useEffect(() => {
-    const body = document.body; 
+    const body = document.body;
     if (modal) {
       body.scrollIntoView();
       body.style.overflowY = 'hidden';
 
-      const information = document.getElementById(`missionsModal-${prop.name}`) as HTMLDialogElement;
+      const information = document.getElementById(`missionsModal-${stamp.name}`) as HTMLDialogElement;
       information.showModal();
-      information.scrollTo({top: 0, left: 0,  behavior: 'instant'});
+      information.scrollTo({ top: 0, left: 0, behavior: 'instant' });
     }
 
-  }, [modal, prop.name])
+  }, [modal, stamp.name])
 
   const stampState = () => {
     if (completedStamps) {
@@ -32,8 +32,8 @@ const SpecificMission = ({ prop, completedStamps }: Prop) => {
     }
 
     // console.log(prop.name)
-    if (prop.name) {
-      const name = prop.name;
+    if (stamp.name) {
+      const name = stamp.name;
       if (name === 'Stol' || name === 'Sten' || name === 'Banan') return `placeholder-bronze.svg`;
       if (name === 'Gunters Korvar') return `placeholder-silver.svg`;
       if (name === 'Gravsten') return `placeholder-guld.svg`;
@@ -49,9 +49,9 @@ const SpecificMission = ({ prop, completedStamps }: Prop) => {
         className="bg-neturalWhite flex justify-between h-[135px] border-2 border-darkGreen py-2 px-3 rounded-xl">
         <div className="space-y-3 w-4/6 h-max font-bold">
           <p className="text-sm">Uppdrag</p>
-          <p className="text-darkGreen text-xl uppercase">{prop.name}</p>
+          <p className="text-darkGreen text-xl uppercase">{stamp.name}</p>
           <div className="flex mb-2">
-            <StampStats prop={prop} />
+            <StampStats stamp={stamp} />
           </div>
         </div>
         <div className="size-18 self-center" >
@@ -64,7 +64,7 @@ const SpecificMission = ({ prop, completedStamps }: Prop) => {
           }} />
         </div>
       </div>
-      {modal && <Dialog stamp={prop} rarityPlaceholder={rarity} setModal={setModal} />}
+      {modal && <Dialog stamp={stamp} rarityPlaceholder={rarity} setModal={setModal} completedStamps={completedStamps} />}
     </>
   )
 }
